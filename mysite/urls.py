ef432196bef315531538f0a6af4dcf2dddd5e5d4
Keren <proskuneoks777@gmail.com>
2026-09-02 14:05:35 -0400

    Fix media serving with explicit route

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,24 +15,21 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
 from my_page import views
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),  # Ruta de administración
-    path('', include('my_page.urls')),  # Incluye las URLs de my_page
+    path('admin/', admin.site.urls),
+    path('', include('my_page.urls')),
     path('download/<int:design_id>/', views.download_vector, name='download_vector'),
     path('accounts/', include('allauth.urls')),
-    
- ]
 
-
-
-urlpatterns += static(
-    settings.MEDIA_URL,
-    document_root=settings.MEDIA_ROOT,
-    insecure=True
-)
+    re_path(
+        r'^media/(?P<path>.*)$',
+        serve,
+        {'document_root': settings.MEDIA_ROOT},
+    ),
+]
