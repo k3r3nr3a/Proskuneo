085350e9f8623854cae127c5b52eb6016917f4dc
Keren <proskuneoks777@gmail.com>
2026-09-20 12:25:42 -0400

    Configure Resend email backend

--- mysite/settings.py
+++ mysite/settings.py
@@ -166,14 +166,15 @@ STORAGES = {
     },
 }
 
-# --- EMAIL / MAILTRAP ---
-# --- EMAIL / GMAIL ---
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-EMAIL_USE_TLS = True
+# --- EMAIL / RESEND ---
+EMAIL_BACKEND = "anymail.backends.resend.EmailBackend"
+
+ANYMAIL = {
+    "RESEND_API_KEY": os.getenv("RESEND_API_KEY"),
+}
+
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
+SERVER_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
 
 # --- OTROS ---
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
