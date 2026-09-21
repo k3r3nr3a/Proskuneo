d3bc2ba2ea5a9b90a31a93bec25d71baee4e9331
Keren <proskuneoks777@gmail.com>
2026-09-21 11:54:08 -0400

    Allow local and Render hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,7 +11,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # --- SECURITY ---
 SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+# ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'localhost',
+    'proskuneo.onrender.com',
+]
 CSRF_TRUSTED_ORIGINS = [
     'https://proskuneo.onrender.com',
 ]
